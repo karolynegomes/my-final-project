1740972152f3661b1020187e446bec5def309797
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2023-11-14 15:46:08 +0200

    Simple test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  /*render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(linkElement).toBeInTheDocument();*/
+  const checkText = jest.fn();
+  render (<BookForm onSubmit={checkText}/>)
+
+  const checkLabel = screen.getByLabelText(/Name/);
+  expect(checkLabel).toBeInTheDocument();
+  })
